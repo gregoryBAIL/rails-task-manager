f04318127fa652d7d897a83a722e590a012ef087
gregory bail <gregory.bail@cogeli.fr>
2023-06-13 15:39:01 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # get 'tasks/index'
+  # get 'tasks/create'
+  # get 'tasks/new'
+  # get 'tasks/destroy'
+  # get 'tasks/update'
+  # get 'tasks/edit'
+  # get 'tasks/show'
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
